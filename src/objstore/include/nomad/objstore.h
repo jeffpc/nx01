80006038350ab6d61039180f336ed640a22fbb3b
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2015-06-28 21:57:06 -0400

    objstore: initial mockup of object store shared library
    The idea is to abstract all the details of volume and volume group
    management in a shared library.  Then, both the client and server daemons
    can use the same library to manage their respective local storage.
    
    The object store will have two components: (1) a generic API that consumers
    use, and (2) an assortment of backends responsible for persistently storing
    objects.
    
    Currently, there are no backends.
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

---
+++ src/objstore/include/nomad/objstore.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015 Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __NOMAD_OBJSTORE_H
+#define __NOMAD_OBJSTORE_H
+
+#include <nomad/types.h>
+
+enum objstore_mode {
+	OS_MODE_CACHE,
+	OS_MODE_STORE,
+};
+
+struct objstore_def;
+
+struct objstore {
+	const struct objstore_def *def;
+
+	struct nuuid uuid;
+	const char *path;
+	enum objstore_mode mode;
+};
+
+extern void objstore_init(void);
+extern struct objstore *objstore_store_create(const char *path);
+extern struct objstore *objstore_store_load(struct nuuid *uuid, const char *path);
+
+#endif
---
+++ src/objstore/include/nomad/objstore_impl.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2015 Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __NOMAD_OBJSTORE_IMPL_H
+#define __NOMAD_OBJSTORE_IMPL_H
+
+#include <nomad/objstore.h>
+
+struct objstore_ops {
+	int (*create)(struct objstore *store);
+	int (*load)(struct objstore *store);
+};
+
+struct obj_ops {
+	int (*getversions)();
+};
+
+struct objstore_def {
+	const char *name;
+	const struct objstore_ops *store_ops;
+	const struct obj_ops *obj_ops;
+};
+
+#endif
