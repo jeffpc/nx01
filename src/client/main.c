1411d6fb954a814014433fe2435a2436bb383b53
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2015-10-17 19:05:15 -0400

    client: RPC dispatching code with a mostly correct NOP fs RPC
    Technically, the NOP RPC is not correct because it doesn't send back the
    command response header.
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- src/client/main.c
+++ src/client/main.c
@@ -28,12 +28,75 @@
 #include <nomad/types.h>
 #include <nomad/objstore.h>
 #include <nomad/connsvc.h>
+#include <nomad/rpc_fs.h>
 
 #define CLIENT_DAEMON_PORT	2323
+
+#define MAP_ERRNO(errno)		\
+	case errno:			\
+		cmd.err = NERR_##errno;	\
+		break
+
+static bool send_response(XDR *xdr, int fd, int err)
+{
+	struct rpc_header_res cmd;
+	int ret;
+
+	switch (err) {
+		MAP_ERRNO(ENOENT);
+		MAP_ERRNO(EEXIST);
+		case 0:
+			cmd.err = NERR_SUCCESS;
+			break;
+		default:
+			fprintf(stderr, "%s cannot map errno %d (%s) to NERR_*\n",
+				__func__, err, strerror(err));
+			cmd.err = NERR_UNKNOWN_ERROR;
+			break;
+	}
+
+	xdr_destroy(xdr);
+	xdrfd_create(xdr, fd, XDR_ENCODE);
+
+	ret = xdr_rpc_header_res(xdr, &cmd);
+
+	return ret ? true : false;
+}
+
+static bool process_command(int fd)
+{
+	struct rpc_header_req cmd;
+	bool ok = false;
+	XDR xdr;
+
+	xdrfd_create(&xdr, fd, XDR_DECODE);
+
+	if (!xdr_rpc_header_req(&xdr, &cmd))
+		goto err;
+
+	printf("got opcode %u\n", cmd.opcode);
+
+	switch (cmd.opcode) {
+		case NRPC_NOP:
+			ok = true;
+			break;
+		default:
+			send_response(&xdr, fd, ENOTSUP);
+			break;
+	}
+
+err:
+	xdr_destroy(&xdr);
+
+	return ok;
+}
 
 static void process_connection(int fd, void *arg)
 {
 	printf("%s: fd = %d, arg = %p\n", __func__, fd, arg);
+
+	while (process_command(fd))
+		;
 
 	close(fd);
 }
